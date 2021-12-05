cb7583dbf816b816ed4fab29e9fbcdaa9bcaaf74
nedumd <davidsonchinedum@gmail.com>
2021-12-05 15:22:47 +0100

    fixed layout

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
       {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1> }
 
 
-      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
+      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-2">
         {images.map(image => (
           <ImageCard key={image.id} image={image} />
         ))}
