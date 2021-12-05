a4e9fcfa72d696673fe2947915597b874db7dd33
nedumd <davidsonchinedum@gmail.com>
2021-12-05 14:56:27 +0100

    minor fix

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ImageCard = ({ image }) => {
-  const tags = image.tags.split(',');
+  // const tags = image.tags.split(',');
 
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
@@ -9,7 +9,7 @@ const ImageCard = ({ image }) => {
       className="w-full" />
       <div className="px-6 py-4">
         <div className="font-bold text-purple-500 text-xl mb-2">
-          Photo by {image.user}
+          Image by {image.user}
         </div>
         <ul>
           <li>
@@ -26,13 +26,14 @@ const ImageCard = ({ image }) => {
           </li>
         </ul>
       </div>
-      <div className="px-6 py-4">
+      {/* <div className="px-6 py-4">
         {tags.map(( tag, index) => (
-          <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          <span key={index} className="inline-block bg-gray-200 rounded-full px-3
+            py-1 text-sm font-semibold text-gray-700 mr-2">
           #{tag}
           </span>
         ))}
-      </div>
+      </div> */}
     </div>
   )
 }
